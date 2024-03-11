4a1744a926493b06d5bd7679ea35dc88d18c56e9
matthewtqs <matthewtqs95@gmail.com>
2024-03-11 11:05:31 +0800

    .

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,15 @@ const app = express()
 
 app.use(express.json())
 app.use("/user", user.router)
+console.log("hello")
 // app.get('/', function(req, res) {
 //     res.send("Hello World")
 // }
 // )
 
-app.listen(3000, (errors) => {
-    if (errors)
-        console.error("not working")
-    else
-    console.log("Listening on Port 3000")
-})
+// app.listen(3000, (errors) => {
+//     if (errors)
+//         console.error("not working")
+//     else
+//     console.log("Listening on Port 3000")
+// })
