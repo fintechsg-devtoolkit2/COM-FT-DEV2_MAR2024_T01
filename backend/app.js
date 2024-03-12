f0af9963ea2fedfbc2f8df0fce8aa49d55f33eca
Darren <zen_darren@hotmail.com>
2024-03-12 21:24:04 +0800

    added cors()

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,16 @@
 const express = require("express");
-const user = require("./src/api/user")
-const app = express()
+const user = require("./src/api/user");
+const app = express();
 
-app.use(express.json())
-app.use("/user", user.router)
+app.use(cors());
+app.use(express.json());
+app.use("/user", user.router);
 // app.get('/', function(req, res) {
 //     res.send("Hello World")
 // }
 // )
 
 app.listen(3000, (errors) => {
-    if (errors)
-        console.error("not working")
-    else
-    console.log("Listening on Port 3000")
-})
+  if (errors) console.error("not working");
+  else console.log("Listening on Port 3000");
+});
