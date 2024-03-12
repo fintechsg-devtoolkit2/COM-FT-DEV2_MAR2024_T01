7cbc23b69c6d01d8b51195f777a7ba9b8b61a23e
matthewtqs <162071550+matthewtqs@users.noreply.github.com>
2024-03-13 05:28:14 +0800

    Update subscription.js

--- backend/src/api/subscription.js
+++ backend/src/api/subscription.js
@@ -5,27 +5,7 @@ const routerSubscription = express.Router();
 // API endpoint to handle POST requests with email payload
 routerSubscription.post("/", (req, res) => {
 
-  // if (!email) {
-  //   return res.status(400).json({ error: "Email is required" });
-  // }
-
-  // dummy user
-//   const user = {
-//     id: 1,
-//     name: "Jane Doe",
-//     email: "jane@example.com",
-//     planType: "Premium",
-//   };
-
-//   if (!user) {
-//     return res.status(404).json({ error: "User not found" });
-//   }
-
-//   // Return dummy user details
-//   return res.json(User added);
-// });
-
 return res.json({ Success: "POSTresponse" })
-}
+});
                         
 module.exports = { routerSubscription };
