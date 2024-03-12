eac41b79e3ed265de9a4cd0c1e8d9d7f06b7c4a9
Darren <zen_darren@hotmail.com>
2024-03-12 21:30:13 +0800

    add retrieveUserDetails

--- frontend/public/js/scripts.js
+++ frontend/public/js/scripts.js
@@ -1,3 +1,22 @@
+function retrieveUserDetails(formData) {
+  console.log(formData);
+
+  var settings = {
+    url: "https://fintechsg-devtoolkit2-t01.onrender.com/user/details",
+    method: "POST",
+    timeout: 0,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    data: JSON.stringify(formData),
+  };
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+}
+
 function submitSubscription(event) {
   // var usernameValue = $("#username").val(); // Get value of username input
   // var emailValue = $("#email").val(); // Get value of email input
@@ -5,18 +24,7 @@ function submitSubscription(event) {
 
   //   let url = "https://fe04156e-b0a5-468a-a569-dab0a548bb56.mock.pstmn.io";
 
-  //   var settings = {
-  //     url: `${url}/transactions?startdate=${startdate}&enddate=${enddate}`,
-  //     method: "GET",
-  //     timeout: 0,
-  //     headers: {
-  //       Accept: "application/json",
-  //     },
-  //   };
-
   //   var myArray = [];
-  //   console.log("calling API");
-
   //   $.ajax(settings).done(function (response) {
   //     myArray = response;
   //     buildTable(myArray);
@@ -38,5 +46,19 @@ $(document).ready(function () {
     });
 
     console.log(formData);
+  });
+
+  $("#detailsForm").submit(function (e) {
+    e.preventDefault(); // Prevent form submission
+    var serializedData = $(this).serialize(); // Serialize form data
+
+    var formData = {};
+
+    serializedData.split("&").forEach(function (item) {
+      var keyValue = item.split("=");
+      formData[keyValue[0]] = decodeURIComponent(keyValue[1] || "");
+    });
+
+    retrieveUserDetails(formData);
   });
 });
